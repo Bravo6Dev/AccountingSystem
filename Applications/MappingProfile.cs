9f1ca7cc4df4fb8141b10bb5ef4a5c5f2e7b931b
Hatem <beshinahatem@gmail.com>
2025-06-11 18:25:10 +0200

    mapping employee dto to employee

--- Applications/MappingProfile.cs
+++ Applications/MappingProfile.cs
@@ -23,6 +23,10 @@ namespace Applications
                 .ForMember(dest => dest.CreateDate, opt => opt.MapFrom(_ => DateTime.Now))
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(_ => DataStatus.Active)) 
                 .ForMember(dest => dest.CashBox, opt => opt.MapFrom(src => src.CashBox));
+
+            CreateMap<Employee, EmployeeDto>()
+                .ForMember(dest => dest.Permission, opt => opt.MapFrom(src => src.Permission.Name))
+                .ForMember(dest => dest.CashBoxName, opt => opt.MapFrom(src => src.CashBox != null ? src.CashBox.Name : string.Empty));
         }
     }
 }
